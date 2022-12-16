c33d5e727e1ad6797fe6a915035361d87a495a63
suleymanozkeskin <75538999+suleymanozkeskin@users.noreply.github.com>
2022-12-16 16:26:51 +0100

    updates

--- source/main.js
+++ source/main.js
@@ -42,5 +42,5 @@ function showSpinner() {
 function hideSpinner() {
   const button = document.querySelector('button');
   button.disabled = false;
-  button.innerHTML = 'Dream';
+  button.innerHTML = 'Generate';
 }
